806e4b40d1ab23ac22a6229d56ea3ae77045146a
Akbar Alam <alam.akbar1989@gmail.com>
2023-04-24 18:15:02 +0000

    week:2 cloudwatch logging complete

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -51,7 +51,7 @@ from time import strftime
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+cw_handler = watchtower.CloudWatchLogHandler(log_group='backend-cruddur')
 LOGGER.addHandler(console_handler)
 LOGGER.addHandler(cw_handler)
 LOGGER.info("test logging")
