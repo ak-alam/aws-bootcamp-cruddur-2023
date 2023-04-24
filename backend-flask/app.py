0c6c8fc332d5081a74dbfeef1945e6253bf091cc
Akbar Alam <alam.akbar1989@gmail.com>
2023-04-24 19:27:27 +0000

    week:2 rollbar logging implementation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -58,11 +58,11 @@ from services.show_activity import *
 
 
 #Rollbar logging
-import rollbar
-import rollbar.contrib.flask
-from flask import got_request_exception
+# import rollbar
+# import rollbar.contrib.flask
+# from flask import got_request_exception
 
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 
 app = Flask(__name__)
 
@@ -74,21 +74,21 @@ app = Flask(__name__)
 # XRayMiddleware(app, xray_recorder)
 
 #Rollbar
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'development',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token,
+#         # environment name
+#         'development',
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -152,7 +152,9 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run(logger=LOGGER)
+  data = HomeActivities.run()
+  # data = HomeActivities.run(logger=LOGGER)
+
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
